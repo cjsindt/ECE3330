38cde35dbd54dc4f1ef66ace3b18c1928b1bb794
cjsindt <connor-sindt@uiowa.edu>
2020-09-08 16:57:02 -0500

    PostnetEncoder working

--- oral_exam1/S3_Barcodes/src/PostnetEncoder.java
+++ oral_exam1/S3_Barcodes/src/PostnetEncoder.java
@@ -1,48 +1,126 @@
+/**
+ * Class to encode a ZIP, ZIP+4, or ZIP+4+Delivery Code into the POSTNET barcode standard.
+ * @author cjsindt
+ * @version 1.0.0, 8 Sept 2020
+ */
 public class PostnetEncoder {
 
-    private String zipCode;
-    private String binaryEncoding;
+    private String zipCode = ""; //the zipcode given by the user
+    private String binaryEncoding = "1"; //the binary encoding of the zipcode, always starts with 1
+    private String barEncoding = "|"; //the bar encoding of the zipcode, always starts with |
 
+    //conversion chart from decimal to binary based on index
+    final private static String[] binaryEncodingChart = {"11000", "00011", "00101", "00110", "01001", "01010", "01100", "10001", "10010", "10100"};
+
+    //conversion chart from decimal to bars based on index
+    final private static String[] barEncodingChart = {"||...", "...||", "..|.|", "..||.", ".|..|", ".|.|.", ".||..", "|...|", "|..|.", "|.|.."};
+
+    /**
+     * Default Constructor with no parameters
+     */
     public PostnetEncoder(){
         zipCode = "";
     }
 
+    /**
+     * Constructor that takes a Zip Code as a parameter with hyphens separating the ZIP from the +4 and Delivery code.
+     * @param zip   The Zip Code (ZIP, ZIP+4, ZIP+4+Delivery Code)
+     */
     public PostnetEncoder(String zip){
         setZipCode(zip);
     }
 
-    private void setZipCode(String zip){
-        boolean validZip = true; //assume the zipcode is a valid code
-
-        //the only valid lengths for a zipcode are 5, 10, and 12. Don't test any input that has incorrect length
-        if(zip.length() == 5 || zip.length() == 10 || zip.length() == 12) {
-            //check every character in given string and make sure they are all numeric digits
-            for(int i = 0; i < zip.length(); i++){
-                //if the 6th digit is not a hyphen, not a valid zip
-                //TODO: Fix this line, don't work :(
-                if(i == 5 && zip.charAt(i) != '-'){
-                    validZip = false;
-                }
-                //if the character at the current index is not a digit, the zip is invalid
-                else if(!(Character.isDigit(zip.charAt(i)))){
-                    validZip = false;
-                }
-            }
-        }
-        else{
-            validZip = false;
-        }
-
-        if(validZip) {
+    /**
+     * Sets the Zip code to zip and encodes it into its Binary and POSTNET representations.
+     * @param zip   The Zip Code (ZIP, ZIP+4, ZIP+4+Delivery Code)
+     */
+    public void setZipCode(String zip){
+        if(isValidZip(zip)) {
             zipCode = zip;
-            System.out.println("Valid Zip: " + zip);
-        }
-        else{
-            System.out.println("Invalid Zip: " + zip);
+            encodeZipCode(zip);
+        } else {
+            System.out.println("Invalid Zip Code.");
         }
     }
 
-    private String getZipCode(){
+    /**
+     *
+     * @return
+     */
+    public String getZipCode(){
         return zipCode;
+    }
+
+    public String getBinaryEncoding(){
+        return binaryEncoding;
+    }
+
+    public String getBarEncoding(){
+        return barEncoding;
+    }
+
+    public static boolean isValidZip(String zip){
+
+        //null check
+        if(zip != null) {
+            //zip cannot be null, and the only valid lengths for a zip code are 5, 10, and 12. Don't test any input that has incorrect length
+            if (zip.length() == 5 || zip.length() == 10 || zip.length() == 13) {
+                //check every character in given string and make sure they are all numeric digits
+                for (int i = 0; i < zip.length(); i++) {
+                    //if the 6th  or 11th digit is not a hyphen, not a valid zip
+                    if (i == 5 || i == 10) {
+                        //great, we have a hyphen, add one to i and keep checking numbers
+                        if (zip.charAt(i) == '-') {
+                            i += 1;
+                            //no hyphen = invalid zip
+                        } else {
+                            return false;
+                        }
+                    }
+                    //if the character at the current index is not a digit, the zip is invalid
+                    //can check every time b/c if we have a hyphen, we add one to i
+                    if (!(Character.isDigit(zip.charAt(i)))) {
+                        return false;
+                    }
+                }
+            }
+            //zip code is an invalid length, return false
+            else {
+                return false;
+            }
+
+            //zip code is null, return false
+        } else{
+            return false;
+        }
+
+        return true;
+    }
+
+
+    private void encodeZipCode(String zip){
+
+            //the checksum of the encoding
+            int checkSum = 0;
+
+            for(int i = 0; i < zip.length(); i++){
+                //if there is a hyphen, advance the index to next number
+                if(zip.charAt(i) == '-'){
+                    i+=1;
+                }
+                checkSum += (zip.charAt(i))-48; //add to checkSum the char value offset by 48 (0 in ASCII is 48)
+
+                //convert the char to a number between 0-9 and use that as the index for the encoding chart
+                binaryEncoding += binaryEncodingChart[zip.charAt(i)-48];
+                barEncoding += barEncodingChart[zip.charAt(i)-48];
+            }
+
+            //checkSum value equals 10 - (the sum of the digits modulo 10)
+            checkSum = 10 - (checkSum % 10);
+
+            //add the checkSum encoding to the end, plus an extra 1 or |
+            binaryEncoding += binaryEncodingChart[checkSum] + "1";
+            barEncoding += barEncodingChart[checkSum] + "|";
+
     }
 }
--- oral_exam1/S3_Barcodes/src/PostnetEncoderTester.java
+++ oral_exam1/S3_Barcodes/src/PostnetEncoderTester.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class PostnetEncoderTester {
 
     public static void main(String[] args){
-        PostnetEncoder p = new PostnetEncoder("55555-1234");
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter Zip: ");
+
+        String input = s.nextLine();
+
+        PostnetEncoder p = new PostnetEncoder(input);
+
+        if(PostnetEncoder.isValidZip(p.getZipCode())) {
+            System.out.println(p.getBarEncoding() + "\n" + p.getBinaryEncoding());
+        }
     }
 }
