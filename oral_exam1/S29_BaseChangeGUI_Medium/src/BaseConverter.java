93b2d797c14fc3587f06fc8aee56e5c2d1f11850
cjsindt <connor-sindt@uiowa.edu>
2020-10-07 12:53:43 -0500

    removed unused import statements

--- oral_exam1/S29_BaseChangeGUI_Medium/src/BaseConverter.java
+++ oral_exam1/S29_BaseChangeGUI_Medium/src/BaseConverter.java
@@ -1,9 +1,7 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 
 public class BaseConverter extends JFrame {
 
