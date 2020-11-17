ec7a3af5b2735d420b088d0ebc309920cb2998e0
cjsindt <connor-sindt@uiowa.edu>
2020-11-17 17:00:11 -0600

    working on medium

--- oral_exam2/S26_CannyEdgeDetector/src/EdgeDetector.java
+++ oral_exam2/S26_CannyEdgeDetector/src/EdgeDetector.java
@@ -22,4 +22,58 @@ public class EdgeDetector {
 
         return result;
     }
+
+    public static BufferedImage hysteresis(int[][] img, int width, int height){
+        BufferedImage result = new BufferedImage(width+2, height+2, BufferedImage.TYPE_BYTE_GRAY);
+        boolean[][] trueEdges = new boolean[width+2][height+2]; //state of each pixel, either is or isn't a true edge
+        boolean delta = false; //becomes true if a pixel changes state
+        int low = img[0][0]; //lowest magnitude
+        int high = img[1][1]; //highest magnitude
+
+        for(int[] a : img){ //compute the low and high magnitudes
+            for(int i : a){
+                if(i < high){
+                    high = i;
+                } else if(i > low){
+                    low = i;
+                }
+                System.out.println(i*-1);
+            }
+        }
+
+        System.out.println("High: " + high + " Low: " + low);
+        low = (high*-1)-100;
+
+        System.out.println("High: " + high + " Low: " + low);
+
+        for(int i = 1; i < width+1; i++){
+            for(int j = 1; j < height+1; j++){
+                if(img[i][j]*-1 >= low){
+                    trueEdges[i][j] = true;
+                }
+            }
+        }
+
+        do{
+            delta = false;
+            for(int i = 1; i < width+1; i++){
+                for(int j = 1; j < height+1; j++){
+                    if(img[i][j]*-1 > low && !trueEdges[i][j] && (trueEdges[i-1][j] || trueEdges[i+1][j] || trueEdges[i][j-1] || trueEdges[i][j+1])){
+                        trueEdges[i][j] = true;
+                        delta = true;
+                    }
+                }
+            }
+        } while(delta);
+
+        for(int i = 0; i < width+2; i++){
+            for(int j = 0; j < height+2; j++){
+                if(trueEdges[i][j]){
+                    result.setRGB(i, j, Integer.MAX_VALUE);
+                }
+            }
+        }
+
+        return result;
+    }
 }
--- oral_exam2/S26_CannyEdgeDetector/src/EdgeDetectorDriver.java
+++ oral_exam2/S26_CannyEdgeDetector/src/EdgeDetectorDriver.java
@@ -2,19 +2,38 @@ import javax.swing.*;
 
 public class EdgeDetectorDriver {
     public static void main(String[] args){
-        JFrame j0 = new JFrame("Original Image");
+        JFrame j0 = new JFrame("EASY Original");
         j0.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        ImageConverter img = new ImageConverter("img/EASY_sample_image.bmp");
-        j0.getContentPane().add(new JLabel(new ImageIcon(img.generateImage())));
+        ImageConverter imgEasy = new ImageConverter("img/EASY_sample_image.bmp");
+        j0.getContentPane().add(new JLabel(new ImageIcon(imgEasy.generateImage())));
         j0.pack();
         j0.setVisible(true);
+        j0.setLocation(0,0);
 
-        JFrame j1 = new JFrame("Vertical Edge Detection");
+        JFrame j1 = new JFrame("EASY Edge");
         j1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        j1.getContentPane().add(new JLabel(new ImageIcon(EdgeDetector.detectVerticalEdges(img.getData(), img.getWidth(), img.getHeight()))));
+        j1.getContentPane().add(new JLabel(new ImageIcon(EdgeDetector.detectVerticalEdges(imgEasy.getData(), imgEasy.getWidth(), imgEasy.getHeight()))));
         j1.pack();
         j1.setVisible(true);
+        j1.setLocation(350, 0);
+
+        JFrame j2 = new JFrame("MEDIUM Original");
+        j2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        ImageConverter imgMedium = new ImageConverter("img/MEDIUM_sample_image.bmp");
+        j2.getContentPane().add(new JLabel(new ImageIcon(imgMedium.generateImage())));
+        j2.pack();
+        j2.setVisible(true);
+        j2.setLocation(0,350);
+
+        JFrame j3 = new JFrame("MEDIUM Hysteresis");
+        j3.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        j3.getContentPane().add(new JLabel(new ImageIcon(EdgeDetector.hysteresis(imgMedium.getData(), imgMedium.getWidth(), imgMedium.getHeight()))));
+        j3.pack();
+        j3.setVisible(true);
+        j3.setLocation(350,350);
     }
 }
